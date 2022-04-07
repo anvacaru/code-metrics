91aa6a6495781256a51d870118845978157bf40b
Andrei <andrei.vacaru@runtimeverification.com>
2022-04-07 14:04:07 +0300

    format

--- src/index.js
+++ src/index.js
@@ -1,20 +1,19 @@
 const {SolidityMetricsContainer} = require('solidity-code-metrics');
 
 let options = {
-        basePath:"",
-            inputFileGlobExclusions:undefined,
-                inputFileGlob: undefined,
-                    inputFileGlobLimit: undefined,
-                        debug:false,
-                            repoInfo: {
-                                        branch: undefined,
-                                                commit: undefined,
-                                                        remote: undefined
-                            }
+    basePath:"",
+    inputFileGlobExclusions:undefined,
+    inputFileGlob: undefined,
+    inputFileGlobLimit: undefined,
+    debug:false,
+    repoInfo: {
+        branch: undefined,
+        commit: undefined,
+        remote: undefined
+    }
 }
 
 let metrics = new SolidityMetricsContainer("metricsContainerName", options);
-
 
 process.argv.slice(1,).forEach(f => {
     if(f.endsWith(".sol")){
@@ -27,11 +26,11 @@ console.log("| file | lines | nLines | nSLOC | Comment Lines | Complexity Score 
 console.log("|------|-------|--------|-------|---------------|------------------|")
 
 metrics.metrics.forEach(f => {
-        var message = "| name | "
-            message += f.metrics.sloc.total + " | "
-                message += f.metrics.nsloc.total + " | "
-                    message += f.metrics.nsloc.source + " | "
-                        message += f.metrics.nsloc.comment + " | "
-                            message += f.metrics.complexity.perceivedNaiveScore + " | "
-                                console.log(message)
+    var message = "| name | "
+        message += f.metrics.sloc.total + " | "
+        message += f.metrics.nsloc.total + " | "
+        message += f.metrics.nsloc.source + " | "
+        message += f.metrics.nsloc.comment + " | "
+        message += f.metrics.complexity.perceivedNaiveScore + " | "
+        console.log(message)
 })
